ab38a946f0b51ca54ff4d0ae8b641b351863d914
Dovydas Rupsys <dovydas.rupsys@mvfglobal.com>
2022-02-07 11:11:06 +0000

    Move tomstone loader before codeigniter loader

--- src/Module.php
+++ src/Module.php
@@ -10,14 +10,14 @@ class Module extends \Codeception\Module
 
     public function _initialize()
     {
-        require_once(dirname(__FILE__) . '/Setup.php');
-        $this->ci = get_instance();
-
-        require_once(getcwd() . '/vendor/scheb/tombstone-logger/tombstone-function.php');
+        require_once('vendor/scheb/tombstone-logger/tombstone-function.php');
         $graveyard = new GraveyardBuilder();
         $graveyard->rootDirectory(__DIR__);
         $graveyard->autoRegister();
         $graveyard->build();
+
+        require_once(dirname(__FILE__) . '/Setup.php');
+        $this->ci = get_instance();
     }
 
     public function getCodeIgniterInstance()
